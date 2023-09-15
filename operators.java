45be53677a8b4f1edb993a540c1067a11200f907
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-15 17:56:48 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class operators
+{
+	public static void main(String[] args) {
+  /// operators +, - , %
+     int first = 10;
+     int second = 15;
+     int sum = first + second;
+     /// type casting 
+     double dvd = (double)first/(double)(second);
+   
+
+		System.out.println(sum);
+		/// taking user input 
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		/// use for next like sc.nextfloat(), for string sc.nextline(),
+		
+	
+	
+	}
+}

0ff6c81ef3f54ce627b5d05f6c04295ba6c9c905
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-15 17:41:50 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	//int , double,float,char,String,boolean
+	//rule cant start a variable wit no like 2age
+	int age = 18;
+	/// in float use f 
+	float marks = 16.87f;
+	boolean isvalid = false;
+	/// use " " this in strings
+	String name ="vinayak";
+	char a ='v';
+		System.out.println(age);
+	
+	
+	}
+}
